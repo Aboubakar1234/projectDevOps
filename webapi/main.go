05b6ac72a2794991286a5a69c477aaf0fe7edf4b
Aboubakar Mohamed Abdoulaye <abdoulaye.aboubakar@efrei.net>
2025-01-10 02:41:19 +0100

    ajout nom

--- webapi/main.go
+++ webapi/main.go
@@ -19,7 +19,7 @@ func main() {
 
 func whoAmI(response http.ResponseWriter, r *http.Request) {
 	who := []whoami{
-		whoami{Name: "Efrei Paris",
+		whoami{Name: "Aboubakar mohamed abdoulaye",
 			Title: "DevOps and Continous Deployment",
 			State: "FR",
 		},
